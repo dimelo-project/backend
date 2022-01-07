80ba8c5faa46cead39a98686808612048c119a1d
suyeon <pisuyeun@gmail.com>
2022-01-07 12:21:51 +0900

    feat: users controller 함수 정의(#8)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post('/signup')
+  signup() {}
+
+  @Post('/login')
+  login() {}
+
+  @Post('/logout')
+  logout() {}
+
+  @Post('/check/email')
+  checkEmail() {}
+
+  @Post('/check/nickname')
+  checkNickname() {}
+
+  @Get('/:id')
+  getUserInfo(@Param('id', ParseIntPipe) id: number) {}
+
+  @Get('/me')
+  getMyInfo() {}
+
+  @Patch('/me')
+  updateMyInfo() {}
+
+  @Delete('/me')
+  deleteMyAccount() {}
+
+  @Post('/password')
+  findMyPassword() {}
+
+  @Patch('/password')
+  updateMyPassword() {}
+}
