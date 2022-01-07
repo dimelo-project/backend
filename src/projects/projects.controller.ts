1f2a6f0cd41cc7a3b982c7f2f5edf54309436107
suyeon <pisuyeun@gmail.com>
2022-01-07 14:06:11 +0900

    feat: projects controller 함수 정의(#8)

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  ParseIntPipe,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
-@Controller('projects')
-export class ProjectsController {}
+@Controller('api/projects')
+export class ProjectsController {
+  @Get()
+  getAllProjects(
+    @Query('position') position: string,
+    @Query('skill') skill: string,
+    @Query('ongoing') ongoing: string,
+  ) {}
+
+  @Get('/:id')
+  getProject(@Param('id', ParseIntPipe) id: number) {}
+
+  @Post()
+  createProject() {}
+
+  @Patch('/:id')
+  updateProject(@Param('id', ParseIntPipe) id: number) {}
+
+  @Delete('/:id')
+  deleteProject(@Param('id', ParseIntPipe) id: number) {}
+
+  @Get('/:project_id/comments')
+  getAllCommentsOfProject(
+    @Param('project_id', ParseIntPipe) project_id: number,
+  ) {}
+
+  @Post('/:project_id/comments')
+  createCommentOfProject(
+    @Param('project_id', ParseIntPipe) project_id: number,
+  ) {}
+
+  @Patch('/:project_id/comments/:id')
+  updateCommentOfProject(
+    @Param('project_id', ParseIntPipe) project_id: number,
+    @Param('id', ParseIntPipe) id: number,
+  ) {}
+
+  @Delete('/:project_id/comments/:id')
+  deleteCommentOfProject(
+    @Param('project_id', ParseIntPipe) project_id: number,
+    @Param('id', ParseIntPipe) id: number,
+  ) {}
+}
