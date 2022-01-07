d8dc00cda1d33d3ef88645724845eaa115d8f989
suyeon <pisuyeun@gmail.com>
2022-01-07 12:34:14 +0900

    feat: courses controller 함수 정의(#8)

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+  @Get()
+  getCourses(
+    @Query('big') big: string,
+    @Query('small') small: string,
+    @Query('skill') skill: string,
+    @Query('perPage', ParseIntPipe) perPage: number,
+    @Query('page', ParseIntPipe) page: number,
+  ) {}
+
+  @Get('/:id')
+  getCourse(@Param('id', ParseIntPipe) id: number) {}
+
+  @Get('/search')
+  searchCourse(@Query('keyword') keyword: string) {}
+
+  @Get('/likes/me')
+  getLikedCourses() {}
+
+  @Post('/likes/:id')
+  likeCourse(@Param('id', ParseIntPipe) id: number) {}
+
+  @Delete('/likes/:id')
+  dislikeCourse(@Param('id', ParseIntPipe) id: number) {}
+
+  @Get('/instructor/:instructor_id')
+  getCoursesByInstructor(
+    @Param('instructor_id', ParseIntPipe) instructor_id: number,
+  ) {}
+}
