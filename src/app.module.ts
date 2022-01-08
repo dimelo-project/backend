89f7a92962380f5ae6ceea2d02bc7149a66bf59e
suyeon <pisuyeun@gmail.com>
2022-01-08 15:09:59 +0900

    feat: typeORM nest에 연결(#14)

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,19 @@ import { ReviewsModule } from './reviews/reviews.module';
 import { TalksModule } from './talks/talks.module';
 import { StudiesModule } from './studies/studies.module';
 import { ProjectsModule } from './projects/projects.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from '../ormconfig';
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, CoursesModule, ReviewsModule, TalksModule, StudiesModule, ProjectsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    CoursesModule,
+    ReviewsModule,
+    TalksModule,
+    StudiesModule,
+    ProjectsModule,
+    TypeOrmModule.forRoot(ormconfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
