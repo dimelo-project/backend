c28947b42b768579e7ba8f76ed8000bf0ed98033
suyeon <pisuyeun@gmail.com>
2022-01-25 18:19:28 +0900

    fix: project position entity data type변경(#61)

--- src/entities/Projects.ts
+++ src/entities/Projects.ts
@@ -18,58 +18,29 @@ import { Users } from './Users';
 import { ProjectsComments } from './ProjectsComments';
 import { ProjectsPositions } from './ProjectsPositions';
 import { ProjectsSkills } from './ProjectsSkills';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Index('FK_project_user_idx', ['userId'], {})
 @Index('id_UNIQUE', ['id'], { unique: true })
 @Entity('projects', { schema: 'dimelo' })
 export class Projects extends BaseEntity {
-  @ApiProperty({
-    example: 1,
-    description: 'project id',
-  })
   @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
   id: number;
 
-  @ApiProperty({
-    example: 'dimelo프로젝트 하실 개발자 구합니다',
-    description: '프로젝트 제목',
-  })
   @Column('varchar', { name: 'title', length: 100 })
   title: string;
 
-  @ApiProperty({
-    example: '프론트 뷰, 백엔드 노드 개발자 구합니다',
-    description: '프로젝트 내용',
-  })
   @Column('text', { name: 'content' })
   content: string;
 
-  @ApiProperty({
-    example: '모집중',
-    description: '모집중/모집완료',
-  })
   @Column('enum', { name: 'ongoing', enum: ['모집중', '모집완료'] })
   ongoing: '모집중' | '모집완료';
 
-  @ApiProperty({
-    example: '2022-01-01',
-    description: '모집을 진행하는 날짜',
-  })
   @Column('datetime', { name: 'duedate', nullable: true })
   duedate: Date | null;
 
-  @ApiProperty({
-    example: 2,
-    description: '모집하는 참여자 수',
-  })
   @Column('int', { name: 'participant', nullable: true })
   participant: number | null;
 
-  @ApiProperty({
-    example: 1,
-    description: '글을 작성한 user id',
-  })
   @Column('int', { name: 'user_id' })
   userId: number;
 
