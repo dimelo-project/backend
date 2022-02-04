c2acd26d907262e2a472df904a6b793dfdb07f36
suyeon <pisuyeun@gmail.com>
2022-02-04 15:43:38 +0900

    fix: delete me api 수정(#67)

--- src/auth/passport/local.strategy.ts
+++ src/auth/passport/local.strategy.ts
@@ -12,7 +12,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string, done: CallableFunction) {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('로그인 정보가 잘못되었습니다');
     }
     return done(null, user);
   }
