060a93dbbfcfc2d022473bcb0a2e350a22a5d866
suyeon <pisuyeun@gmail.com>
2022-02-14 15:22:10 +0900

    refactor: image upload 로직 수정(#67)

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -8,29 +8,15 @@ import {
 } from '@nestjs/swagger';
 import { ImagesService } from './images.service';
 import {
+  ConflictException,
   Controller,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
-import multerS3 from 'multer-s3';
 import AWS from 'aws-sdk';
 import { config } from 'dotenv';
 config();
-
-const s3 = new AWS.S3();
-
-export const multerConfig = {
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.AWS_BUCKET_NAME,
-    acl: 'public-read',
-    key: function (request, file, cb) {
-      cb(null, `${Date.now().toString()}-${file.originalname}`);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-};
 
 @ApiTags('IMAGE')
 @Controller('/api/images')
@@ -48,9 +34,29 @@ export class ImagesController {
     description: '프로필 사진을 올릴 때 file로 들어가는 key',
   })
   @ApiConsumes('multipart/form-data')
-  @UseInterceptors(FileInterceptor('image', multerConfig))
   @Post()
-  async uploadImage(@UploadedFile() file: Express.MulterS3.File) {
-    return this.imagesService.upload(file);
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+    try {
+      const objectName = `${Date.now()}_${file.originalname}`;
+      await new AWS.S3()
+        .putObject({
+          Key: objectName,
+          Body: file.buffer,
+          Bucket: process.env.AWS_BUCKET_NAME,
+          ACL: 'public-read',
+        })
+        .promise();
+      const url = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      return { url };
+    } catch (error) {
+      throw new ConflictException('다시 시도해 주세요');
+    }
   }
 }
