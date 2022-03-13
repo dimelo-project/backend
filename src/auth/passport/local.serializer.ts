a4de7bdf1bae0e07ad8f817222eebd18905f6f1f
suyeon <pisuyeun@gmail.com>
2022-03-13 23:52:52 +0900

    refactor: log 정리

--- src/auth/passport/local.serializer.ts
+++ src/auth/passport/local.serializer.ts
@@ -29,7 +29,6 @@ export class LocalSerializer extends PassportSerializer {
         },
       )
       .then((user) => {
-        console.log(user);
         done(null, user);
       })
       .catch((error) => done(error));
